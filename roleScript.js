eaf098c33aaf02ad5d47177252df30d0571ec5c6
parth-faldu <parth123faldu@gmail.com>
2025-03-21 18:31:31 +0530

    first commit

---
+++ roleScript.js
@@ -0,0 +1,13 @@
+var typeData = new Typed(".role", {
+  strings: [
+    "a Full Stack Developer",
+    "a Web Developer",
+    "a Backend Developer",
+    "a Coder",
+    "Parth Faldu",
+  ],
+  loop: true,
+  typeSpeed: 100,
+  backSpeed: 80,
+  backDelay: 1000,
+});
